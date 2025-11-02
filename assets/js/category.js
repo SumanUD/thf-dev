3f7091e0c1c50db3bb9113093b7f2a3ca3d8b225
SumanUD <suman.banik.ursdigitally@gmail.com>
2025-11-02 18:32:34 +0530

    category page redesign

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,32 +1,20 @@
-
 gsap.registerPlugin(ScrollTrigger);
 
 const texts = gsap.utils.toArray(".banner-texts h1");
 
-texts.forEach((text, i) => {
-  gsap.to(text, {
-    scrollTrigger: {
-      trigger: ".video-banner",
-      start: () => `top+=${i * window.innerHeight} top`,
-      end: () => `+=${window.innerHeight}`,
-      scrub: true,
-      pin: true,
-      pinSpacing: false,
-      onEnter: () => showText(i),
-      onEnterBack: () => showText(i),
+let current = 0;
+ScrollTrigger.create({
+  trigger: ".video-banner",
+  start: "top top",
+  end: () => "+=" + (texts.length * window.innerHeight),
+  pin: true,
+  scrub: true,
+  onUpdate: (self) => {
+    let index = Math.round(self.progress * (texts.length - 1));
+    if (index !== current) {
+      texts[current].classList.remove("active");
+      texts[index].classList.add("active");
+      current = index;
     }
-  });
+  },
 });
-
-function showText(index) {
-  texts.forEach((t, i) => {
-    if (i === index) {
-      t.style.opacity = "1";
-      t.style.transform = "translateY(0)";
-    } else {
-      t.style.opacity = "0";
-      t.style.transform = "translateY(50px)";
-    }
-  });
-}
-
